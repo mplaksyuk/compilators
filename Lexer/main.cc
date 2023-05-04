71dc6e6945df0fa1aa29de96df75803b6673a27e
Michael Plaksyuk <mplaksyuk@fire>
2023-05-04 23:10:33 +0300

    print Error/ line and column

--- Lexer/main.cc
+++ Lexer/main.cc
@@ -10,11 +10,17 @@ int main() {
 
     Lexer lex(filename);
     
-    for (auto token = lex.next(); !token.is(Token::Kind::END) && !token.is(Token::Kind::UNEXPECTED); token = lex.next())
+    for (auto token = lex.next(); !token.is(Token::Kind::END); token = lex.next())
     {
-        std::cout << std::setw(12) <<  token.kind() << "\t| " << token.lexeme() << " |\n";
+        if(token.is(Token::Kind::UNEXPECTED)) {
+            std::cerr << std::setw(20) <<  token.kind() << "\t| " << token.lexeme() << "<-- ERROR |\n";
+            std::cout << "line: " << lex.line() << " column: " << lex.column() << " - " << lex.column() + 1 << std::endl;
+            break;
+        }
+        else {
+            std::cout << std::setw(20) <<  token.kind() << "\t| " << token.lexeme() << " |\n";
+        }
     }
-    std::cout << "line: " << lex.line() << " column: " << lex.column() << std::endl;
 
     return 0;
 }
